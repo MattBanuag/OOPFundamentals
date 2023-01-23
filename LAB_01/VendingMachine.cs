a0f85da92df8d55a613f6c6b6fb55c2669b17532
MattBanuag <matthewbanuag.dev@gmail.com>
2023-01-23 14:59:42 -0600

    build: vending machine class

---
+++ LAB_01/VendingMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_01
+{
+    public class VendingMachine
+    {
+        // PROPERTIES
+        public int SerialNumber { get; }
+        public Dictionary<int, int> MoneyFloat { get; }
+        public Dictionary<string, int> Inventory { get; }
+
+        // CONSTRUCTOR
+        public VendingMachine(int serialNumber)
+        {
+            SerialNumber = serialNumber;
+            MoneyFloat = new Dictionary<int, int>();
+            Inventory = new Dictionary<string, int>();  
+        }
+    }
+}
