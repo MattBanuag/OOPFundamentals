3dd15daf68163d2b4cd45b66ac56b5ba65c3adbe
MattBanuag <matthewbanuag.dev@gmail.com>
2023-01-25 17:07:17 -0600

    build: vendItem method

--- LAB_01/Program.cs
+++ LAB_01/Program.cs
@@ -1,39 +1,56 @@
 ﻿// VENDING MACHINE PROGRAM
 // ========== PROGRAM SETUP =========
 using LAB_01;
+using System.Xml.Linq;
 
 // INSTANTIATE VENDING MACHINE
 VendingMachine VM = new VendingMachine(1001);
 
-// INSTANTIATE PRODUCTS
-Product A = new Product("Chocolate bar", 3, "A1");
-Product B = new Product("Bag of chips", 2, "B2");
-Product C = new Product("Soda pop", 3, "C3");
-Product D = new Product("Cup noodles", 4, "D4");
-Product E = new Product("Snackbox", 7, "E5");
-Product F = new Product("Lunch kit", 10, "F6");
-Product G = new Product("Pizza Sub", 6, "G7");
-Product H = new Product("Jar of pickles", 8, "H8");
-
-// PASS PRODUCTS TO 'STOCKITEM' METHOD
+// INSTANTIATE PRODUCTS AND PASS PRODUCTS TO 'STOCKITEM' METHOD
 Console.WriteLine("~ SNACKS AVAILABLE: ");
 Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine(VM.StockItem(A, 13));
-Console.WriteLine(VM.StockItem(B, 7));
-Console.WriteLine(VM.StockItem(C, 6));
-Console.WriteLine(VM.StockItem(D, 9));
-Console.WriteLine(VM.StockItem(E, 5));
-Console.WriteLine(VM.StockItem(F, 3));
-Console.WriteLine(VM.StockItem(G, 8));
-Console.WriteLine(VM.StockItem(H, 2));
+Console.WriteLine(VM.StockItem(new Product("Chocolate bar", 3, "A1"), 13));
+Console.WriteLine(VM.StockItem(new Product("Bag of chips", 2, "A2"), 7));
+Console.WriteLine(VM.StockItem(new Product("Soda pop", 3, "A3"), 6));
+Console.WriteLine(VM.StockItem(new Product("Cup noodles", 4, "A4"), 9));
+Console.WriteLine(VM.StockItem(new Product("Snackbox", 7, "A5"), 5));
+Console.WriteLine(VM.StockItem(new Product("Lunch kit", 10, "A6"), 3));
+Console.WriteLine(VM.StockItem(new Product("Pizza Sub", 6, "A7"), 8));
+Console.WriteLine(VM.StockItem(new Product("Jar of pickles", 8, "A8"), 2));
 Console.ResetColor();
 
 // PASS MONEY TO 'STOCKFLOAT' METHOD
 Console.WriteLine("\n~ CHANGE AVAILABLE: ");
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine(VM.StockFloat(20, 4));
-Console.WriteLine(VM.StockFloat(10, 0));
+Console.WriteLine(VM.StockFloat(10, 2));
 Console.WriteLine(VM.StockFloat(5, 2));
-Console.WriteLine(VM.StockFloat(2, 0));
+Console.WriteLine(VM.StockFloat(2, 3));
 Console.WriteLine(VM.StockFloat(1, 10));
 Console.ResetColor();
+
+// GETTING MONEY INPUT FROM USER AND ITEM OF CHOICE
+int moneyInput = 0;
+string itemChoice = "";
+bool runningTransaction = false;
+
+// VALIDATE IF ITEM EXISTS
+while (!runningTransaction)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"\n~ Please enter your bill: ");
+    Console.ResetColor();
+    moneyInput = Int32.Parse(Console.ReadLine());
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"~ Please enter the item code of your liking: ");
+    Console.ResetColor();
+    itemChoice = Console.ReadLine().ToUpper();
+
+    VM.VendItem(itemChoice, moneyInput);
+}
+
+
+
+
+
--- LAB_01/VendingMachine.cs
+++ LAB_01/VendingMachine.cs
@@ -12,20 +12,20 @@ namespace LAB_01
         // PROPERTIES
         public int SerialNumber { get; }
         public Dictionary<int, int> MoneyFloat { get; }
-        public Dictionary<string, int> Inventory { get; }
+        public Dictionary<Product, int> Inventory { get; }
 
         // CONSTRUCTOR
         public VendingMachine(int serialNumber)
         {
             SerialNumber = serialNumber;
             MoneyFloat = new Dictionary<int, int>();
-            Inventory = new Dictionary<string, int>();  
+            Inventory = new Dictionary<Product, int>();  
         }
 
         // METHODS
         public string StockItem(Product product, int quantity)
         {
-            Inventory[product.ProdCode] = quantity;
+            Inventory[product] = quantity;
             return $"{product.ProdCode} - {product.ProdName} - ${product.ProdPrice}";
         }
 
@@ -35,15 +35,98 @@ namespace LAB_01
             return $"${moneyDenomination}: {quantity} pieces";
         }
 
-        /*public string VendItem(string Product, int quantity) 
+        public void VendItem(string userInput, int moneyInput)
         {
-            if (!Inventory.ContainsKey(Product))
+            int change = 0;
+            bool isRunning = true;
+            foreach (Product item in Inventory.Keys)
             {
-                Console.WriteLine("~ ERROR: This item does not exist.");
-            } else
-            {
-                return String.Join(", ", Inventory[Product]);
+                if (item.ProdCode == userInput && moneyInput >= item.ProdPrice)
+                {
+                    Console.WriteLine($"\n~ FETCHING ITEM {item.ProdName}...");
+                    change = moneyInput - item.ProdPrice;
+                    List<string> changeList = new List<string>();
+
+                    while (change >= 20)
+                    {
+                        if (MoneyFloat[20] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("~ No change available");
+                            Console.WriteLine("~ Cancelling transaction...");
+                            Console.ResetColor();
+                            isRunning = false;
+                        }
+                        changeList.Add("$20");
+                        change -= 20;
+                        MoneyFloat[20]--;
+                    } while (change >= 10)
+                    {
+                        if (MoneyFloat[10] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("~ No change available");
+                            Console.WriteLine("~ Cancelling transaction...");
+                            Console.ResetColor();
+                            isRunning = false;
+                        }
+                        changeList.Add("$10");
+                        change -= 10;
+                        MoneyFloat[10]--;
+                    } while (change >= 5)
+                    {
+                        if (MoneyFloat[5] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("~ No change available");
+                            Console.WriteLine("~ Cancelling transaction...");
+                            Console.ResetColor();
+                            isRunning = false;
+                        }
+                        changeList.Add("$5");
+                        change -= 5;
+                        MoneyFloat[5]--;
+                    } while (change >= 2)
+                    {
+                        if (MoneyFloat[2] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("~ No change available");
+                            Console.WriteLine("~ Cancelling transaction...");
+                            Console.ResetColor();
+                            isRunning = false;
+                        }
+                        changeList.Add("$2");
+                        change -= 2;
+                        MoneyFloat[2]--;
+                    } while (change >= 1)
+                    {
+                        if (MoneyFloat[1] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("~ No change available");
+                            Console.WriteLine("~ Cancelling transaction...");
+                            Console.ResetColor();
+                            isRunning = false;
+                        }
+                        changeList.Add("$1");
+                        change -= 1;
+                        MoneyFloat[1]--;
+                    }
+
+                    // Display change
+                    if (isRunning)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"~ Your change is: {String.Join(", ", changeList)}");
+                        Console.WriteLine();
+                        Console.WriteLine($"~ Enjoy :)\n");
+                        Console.ResetColor();
+                        changeList.Clear();
+                    }
+                    break;
+                }
             }
-        }*/
+        }
     }
 }
